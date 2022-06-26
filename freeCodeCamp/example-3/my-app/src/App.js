41846f64d0fcb6d4cbd42970e0498c692bdbb938
Samuel-P-Moronta <20170570@ce.pucmm.edu.do>
2022-06-26 14:35:39 -0700

    Ahora si

--- freeCodeCamp/example-3/my-app/src/App.js
+++ freeCodeCamp/example-3/my-app/src/App.js
@@ -17,7 +17,11 @@ function App() {
   };
   
   const calcularResultado = () => {
-    setInput(evaluate(input));
+    if(input){
+      setInput(evaluate(input));
+    }else{
+      alert("Ingresar valores!!")
+    }
   };
 
 

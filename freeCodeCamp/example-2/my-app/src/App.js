da83316191815b40ce5670de94ab77daf43d7530
Samuel-P-Moronta <20170570@ce.pucmm.edu.do>
2022-06-26 07:59:17 -0700

    Example 2 done

--- freeCodeCamp/example-2/my-app/src/App.js
+++ freeCodeCamp/example-2/my-app/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import freeCodeCampLogo from './images/freecodecamp-logo.png';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import {useState} from 'react';
+
 function App() {
+
+  const[numClicks, setNumClicks] = useState(0);
+
+
+  const manejarClick = ( ) =>  {
+    setNumClicks(numClicks+1)
+  }
+
+  const reiniciarContador = ( ) => {
+    setNumClicks(0)
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -11,10 +27,23 @@ function App() {
         alt='logo de freeCodeCamp'
         />
       </div>
-
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks}/>
+        <Boton
+          texto="Click"
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
+
 
 export default App;
 
